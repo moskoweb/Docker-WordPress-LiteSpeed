cec67554d2a0c184d3143cc4c8f3782f671cbcb4
Alan Mosko <falecom@alanmosko.com.br>
2020-06-22 23:17:41 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,25 +3,33 @@ set -euo pipefail
 
 /usr/local/lsws/bin/openlitespeed -d
 
-mysql -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASS -e "create database '$WORDPRESS_DB_NAME'"
+mysql --no-defaults -h $WORDPRESS_DB_HOST --port $WORDPRESS_DB_PORT -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASS -e "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;"
 
 cd /var/www/vhosts/localhost/html
 
-wp core download --locale=pt_BR --allow-root
+wp core download --locale=$WP_LOCALE --allow-root
 
-wp core config --allow-root \
-    --dbname=$WORDPRESS_DB_NAME \
-    --dbuser=$WORDPRESS_DB_USER \
-    --dbpass=$WORDPRESS_DB_PASS \
-    --dbhost=$WORDPRESS_DB_HOST \
-    --dbprefix=$WORDPRESS_TPREFIX
+wp config shuffle-salts
 
-wp core install --allow-root \
-    --url=$VIRTUAL_HOST \
-    --title="WordPress com Litespeed" \
-    --admin_user=$WORDPRESS_AD_USER \
-    --admin_password=$WORDPRESS_AD_PASS \
-    --admin_email=$WORDPRESS_AD_MAIL
+wp config set WP_SITEURL "https://$VIRTUAL_HOST" --add --type=constant --allow-root
+wp config set WP_HOME "https://$VIRTUAL_HOST" --add --type=constant --allow-root
+wp config set DB_NAME $WORDPRESS_DB_NAME --add --type=constant --allow-root
+wp config set DB_USER $WORDPRESS_DB_USER --add --type=constant --allow-root
+wp config set DB_PASSWORD $WORDPRESS_DB_PASS --add --type=constant --allow-root
+wp config set DB_HOST "$WORDPRESS_DB_HOST:$WORDPRESS_DB_PORT" --add --type=constant --allow-root
+wp config set DB_PORT $WORDPRESS_DB_PORT --raw --add --type=constant --allow-root
+
+if ! $(wp core is-installed); then
+    wp core install --url=$VIRTUAL_HOST \
+      --title="WordPress com Litespeed" \
+      --admin_user=$WORDPRESS_AD_USER \
+      --admin_password=$WORDPRESS_AD_PASS \
+      --admin_email=$WORDPRESS_AD_MAIL \
+      --skip-email \
+  else
+  fi
+
+wp rewrite structure '/%postname%/'
 
 wp plugin install --allow-root litespeed-cache
 
