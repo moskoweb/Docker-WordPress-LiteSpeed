8de2fc9bf052031e8f9414494914793c28bbf3d3
Alan Mosko <falecom@alanmosko.com.br>
2020-06-22 23:43:26 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,29 +7,41 @@ mysql --no-defaults -h $WORDPRESS_DB_HOST --port $WORDPRESS_DB_PORT -u $WORDPRES
 
 cd /var/www/vhosts/localhost/html
 
-wp core download --locale=$WP_LOCALE --allow-root
+if [ ! -e wp-config.php ]; then
 
-wp config shuffle-salts
+    wp core download --locale=$WP_LOCALE --allow-root
 
-wp config set WP_SITEURL "https://$VIRTUAL_HOST" --add --type=constant --allow-root
-wp config set WP_HOME "https://$VIRTUAL_HOST" --add --type=constant --allow-root
-wp config set DB_NAME $WORDPRESS_DB_NAME --add --type=constant --allow-root
-wp config set DB_USER $WORDPRESS_DB_USER --add --type=constant --allow-root
-wp config set DB_PASSWORD $WORDPRESS_DB_PASS --add --type=constant --allow-root
-wp config set DB_HOST "$WORDPRESS_DB_HOST:$WORDPRESS_DB_PORT" --add --type=constant --allow-root
-wp config set DB_PORT $WORDPRESS_DB_PORT --raw --add --type=constant --allow-root
+    cp wp-config-sample.php wp-config.php
 
-if ! $(wp core is-installed); then
-    wp core install --url=$VIRTUAL_HOST \
-      --title="WordPress com Litespeed" \
-      --admin_user=$WORDPRESS_AD_USER \
-      --admin_password=$WORDPRESS_AD_PASS \
-      --admin_email=$WORDPRESS_AD_MAIL \
-      --skip-email \
-  else
-  fi
+    wp config shuffle-salts
 
-wp rewrite structure '/%postname%/'
+    wp config set WP_SITEURL "https://$VIRTUAL_HOST" --add --type=constant --allow-root
+    wp config set WP_HOME "https://$VIRTUAL_HOST" --add --type=constant --allow-root
+    wp config set DB_NAME $WORDPRESS_DB_NAME --add --type=constant --allow-root
+    wp config set DB_USER $WORDPRESS_DB_USER --add --type=constant --allow-root
+    wp config set DB_PASSWORD $WORDPRESS_DB_PASS --add --type=constant --allow-root
+    wp config set DB_HOST "$WORDPRESS_DB_HOST:$WORDPRESS_DB_PORT" --add --type=constant --allow-root
+    wp config set DB_PORT $WORDPRESS_DB_PORT --raw --add --type=constant --allow-root
+
+    if ! $(wp core is-installed); then
+
+        wp core install --url=$VIRTUAL_HOST \
+        --title="WordPress com Litespeed" \
+        --admin_user=$WORDPRESS_AD_USER \
+        --admin_password=$WORDPRESS_AD_PASS \
+        --admin_email=$WORDPRESS_AD_MAIL \
+        --skip-email \
+
+    else
+    fi
+    
+    wp rewrite structure '/%postname%/'
+
+else
+
+  echo 'wp-config.php file already exists.'
+
+fi
 
 wp plugin install --allow-root litespeed-cache
 
