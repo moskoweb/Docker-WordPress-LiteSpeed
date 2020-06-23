0b96b9f3b9ed580442a1b53e0b1e7affd3a73f2d
Alan Mosko <falecom@alanmosko.com.br>
2020-06-23 00:21:30 -0300

    Update and rename docker-entrypoint.sh to wp-install.sh

---
+++ wp-install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "MySQL | Create database"
+mysql --no-defaults -h $WORDPRESS_DB_HOST --port $WORDPRESS_DB_PORT -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASS -e "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME;"
+
+cd /var/www/vhosts/localhost/html
+
+if [ ! -e wp-config.php ]; then
+    echo "WordPress | Core Download"
+    wp core download --locale=$WP_LOCALE --allow-root
+
+    echo "WordPress | Core Configuration"
+    wp core config --allow-root \
+    --dbname=$WORDPRESS_DB_NAME \
+    --dbuser=$WORDPRESS_DB_USER \
+    --dbpass=$WORDPRESS_DB_PASS \
+    --dbhost="$WORDPRESS_DB_HOST:$WORDPRESS_DB_PORT"
+
+    echo "WordPress | Config Salts"
+    wp config shuffle-salts --allow-root
+
+    if ! $(wp core is-installed --allow-root); then
+
+        echo "WordPress | Core Install"
+        wp core install --url=$VIRTUAL_HOST \
+        --title="WordPress com Litespeed" \
+        --admin_user=$WORDPRESS_AD_USER \
+        --admin_password=$WORDPRESS_AD_PASS \
+        --admin_email=$WORDPRESS_AD_MAIL \
+        --skip-email --allow-root
+    fi
+
+    echo "WordPress | Rewrite Set"
+    wp rewrite structure '/%postname%/' --allow-root
+
+fi
+
+echo "WordPress | Plugin Install"
+wp plugin install --allow-root litespeed-cache
+
+echo "WordPress | Plugin Activate"
+wp plugin activate --allow-root litespeed-cache
