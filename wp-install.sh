413076434acbbc12f5418a2ce2b8d30dd5e3afe9
Alan Mosko <moskoweb@gmail.com>
2020-06-24 00:51:44 -0300

    wip

--- wp-install.sh
+++ wp-install.sh
@@ -86,6 +86,14 @@ chown -R lsadm:lsadm *
 
 chmod -R g+rw *
 
+echo "PHP | Ini Set Values"
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = $PHP_INI_MAXFILE_SIZE/g" /usr/local/lsws/lsphp74/etc/php/7.4/litespeed/php.ini
+sed -i "s/post_max_size = 8M/post_max_size = $PHP_INI_MAXFILE_SIZE/g" /usr/local/lsws/lsphp74/etc/php/7.4/litespeed/php.ini
+sed -i "s/max_execution_time = 30/max_execution_time = $PHP_INI_EXECUTION_TIME/g" /usr/local/lsws/lsphp74/etc/php/7.4/litespeed/php.ini
+sed -i "s/max_input_time = 60/max_input_time = $PHP_INI_EXECUTION_TIME/g" /usr/local/lsws/lsphp74/etc/php/7.4/litespeed/php.ini
+sed -i "s/memory_limit = 128M/memory_limit = $PHP_INI_MEMORY_LIMIT/g" /usr/local/lsws/lsphp74/etc/php/7.4/litespeed/php.ini
+sed -i "s/;max_input_vars = 1000/max_input_vars = 3000/g" /usr/local/lsws/lsphp74/etc/php/7.4/litespeed/php.ini
+
 /usr/local/lsws/bin/lswsctrl restart
 
 echo "WordPress | Install Completed"
